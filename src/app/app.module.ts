a65e43db177ee79d28c6c9e2270cf09dfceadd14
Albert <johan50693@gmail.com>
2021-01-28 19:13:38 -0400

    se agrega grafico lineal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ChartsModule,ThemeService  } from 'ng2-charts';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LineasComponent } from './components/lineas/lineas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule
   ],
-  providers: [],
+  providers: [ThemeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

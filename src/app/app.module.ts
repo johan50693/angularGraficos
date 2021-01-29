e053bb9689d89bf644634132f1c8bd80d2b9d984
Albert <johan50693@gmail.com>
2021-01-29 06:44:13 -0400

    se agrega grafica lineal y de barra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { ChartsModule,ThemeService  } from 'ng2-charts';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LineasComponent } from './components/lineas/lineas.component';
+import { BarraComponent } from './components/barra/barra.component';
+import { DonaComponent } from './components/dona/dona.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LineasComponent
+    LineasComponent,
+    BarraComponent,
+    DonaComponent
   ],
   imports: [
     BrowserModule,
